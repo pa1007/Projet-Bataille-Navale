1e62b4f18564fa0be8365e86b05cc80d5be7d5b7
pa1007 <pa.fourriere@pa1007.fr>
2019-03-14 11:08:31 +0100

    Creation des bateaux avec l'interface

---
+++ src/bateaux/Bateaux.java
@@ -0,0 +1,12 @@
+package bateaux;
+
+public interface Bateaux {
+
+    /**
+     * Pour avoir le nombre de fois que l'objet est present sur la grille( par joueur)
+     *
+     * @return an int du nombre de fois present dans le jeu
+     */
+    public int getNumber();
+
+}
---
+++ src/bateaux/PorteAvion.java
@@ -0,0 +1,14 @@
+package bateaux;
+
+public class PorteAvion implements Bateaux {
+
+    /**
+     * Pour avoir le nombre de fois que l'objet est present sur la grille( par joueur)
+     *
+     * @return an int du nombre de fois present dans le jeu
+     */
+    @Override
+    public int getNumber() {
+        return 0;
+    }
+}
