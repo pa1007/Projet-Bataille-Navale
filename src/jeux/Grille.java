4defd6159925342cb957dfa366df65208a3d3780
pa1007 <pa.fourriere@sfr.fr>
2019-03-14 18:47:29 +0100

    Change constructeur, add une methode pour ecrire au format de la console

--- src/jeux/Grille.java
+++ src/jeux/Grille.java
@@ -13,9 +13,15 @@ public class Grille {
      */
     private int horizontal;
 
-    public Grille(int verticale, int horizontal) {
+    /**
+     * Le parametre permetant de relier je jeux avec la grille
+     */
+    private Jeux jeux;
+
+    public Grille(int verticale, int horizontal, Jeux jeux) {
         this.verticale = verticale;
         this.horizontal = horizontal;
+        this.jeux = jeux;
     }
 
     /**
@@ -48,5 +54,37 @@ public class Grille {
      */
     public void setHorizontal(int horizontal) {
         this.horizontal = horizontal;
+    }
+
+    private String consolForamt() {
+        char[]        alpha = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        StringBuilder sb    = new StringBuilder("   ");
+        int           t     = horizontal, sL = -1;
+        for (int i = 0; i < t; i++) {
+            if (i > 25) {
+                t -= 25;
+                i = 0;
+                sL++;
+            }
+            if (sL != -1) {
+                sb.append(alpha[sL]);
+            }
+            sb.append(alpha[i]).append(" ");
+        }
+        sb.append("\n");
+        for (int i = 1; i < verticale + 1; i++) {
+            sb.append(i < 10 ? "0" + i : i);
+            for (int j = 0; j < horizontal; j++) {
+                sb.append(jeux.getPlace(i, j) == null ? " x" : jeux.getPlace(i, j));
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+
+    }
+
+    @Override
+    public String toString() {
+        return consolForamt();
     }
 }
--- src/jeux/Jeux.java
+++ src/jeux/Jeux.java
@@ -1,15 +1,58 @@
 package jeux;
 
+import bateaux.Bateaux;
+import utils.Player;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Jeux {
+
+    /**
+     * La map princiaple pour avoir un bateau.
+     */
+    private Map<Player, List<Bateaux>> mapPlayerListBateaux;
+
+    public Jeux() {
+        mapPlayerListBateaux = new HashMap<>();
+    }
+
+    /**
+     * @return La map princiaple pour avoir un bateau.
+     */
+    public Map<Player, List<Bateaux>> getMapPlayerListBateaux() {
+        return this.mapPlayerListBateaux;
+    }
+
+    /**
+     * Sets the <code>mapPlayerListBateaux</code> field.
+     *
+     * @param mapPlayerListBateaux La map princiaple pour avoir un bateau.
+     */
+    public void setMapPlayerListBateaux(Map<Player, List<Bateaux>> mapPlayerListBateaux) {
+        this.mapPlayerListBateaux = mapPlayerListBateaux;
+    }
+
+
+    public Bateaux getPlace(int verticale, int horizontal) {
+        Place p = new Place(verticale, horizontal);
+        for (List<Bateaux> bat : mapPlayerListBateaux.values()) {
+            for (Bateaux b : bat) {
+                if (b.getPlaces().contains(p)) {
+                    return b;
+                }
+            }
+        }
+        return null;
+    }
 
 
     /**
      * methode statique qui lance un menu interactif qui permet de choisir les différents mode de jeux
      * et d'accéder aux paramètres
      */
-    public static void menuInteractif(){
+    public static void menuInteractif() {
         int choix;
 
         Scanner sc = new Scanner(System.in);
@@ -19,7 +62,7 @@ public class Jeux {
         System.out.println("3 : Choix de la taille de la grille");
         choix = sc.nextInt();
 
-        switch (choix){
+        switch (choix) {
             case 1:
 
                 break;
@@ -32,27 +75,25 @@ public class Jeux {
                 Jeux.menuInteractif();
                 break;
 
-                default:
+            default:
 
         }
     }
 
 
-
-
-
     /**
      * methode statique qui permet au joueur de modifier la taille de la grille avant de jouer
      */
-    public static void modifTailleGrille(){
+    public static void modifTailleGrille() {
         Scanner sc = new Scanner(System.in);
-            System.out.println("Nombre de case horizontales : ");
-            int horizontale = sc.nextInt();
+        Jeux    j  = new Jeux(); // ajout de ça,faudra modif todo modif
+        System.out.println("Nombre de case horizontales : ");
+        int horizontale = sc.nextInt();
 
-            System.out.println("Nombre de case verticales : ");
-            int verticale = sc.nextInt();
+        System.out.println("Nombre de case verticales : ");
+        int verticale = sc.nextInt();
 
-            Grille g = new Grille(horizontale, verticale);
+        Grille g = new Grille(horizontale, verticale, j);
     }
 
 
@@ -60,7 +101,9 @@ public class Jeux {
      * methode statique qui nettoie la console pour une meilleure lisibilité
      */
     public static void clearScreen() {
-        for (int i = 0; i < 50; ++i) System.out.println();
+        for (int i = 0; i < 50; ++i) {
+            System.out.println();
+        }
     }
 }
 
