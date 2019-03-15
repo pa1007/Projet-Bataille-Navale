a220625390eef3dad061eb02ebc5c33d41857e28
pa1007 <pa.fourriere@pa1007.fr>
2019-03-15 08:39:25 +0100

    Player

--- src/utils/Player.java
+++ src/utils/Player.java
@@ -2,7 +2,36 @@ package utils;
 
 public class Player {
 
-    public Player ( ) {
+    /**
+     * Si le joueur est une IA.
+     *
+     * @since 1.0
+     */
+    private boolean AI;
+
+    public Player() {
         System.out.println();
+    }
+
+    public void placerBateau() {
+
+    }
+
+    /**
+     * @return Si le joueur est une IA.
+     * @since 1.0
+     */
+    public boolean getAI() {
+        return this.AI;
+    }
+
+    /**
+     * Sets the <code>AI</code> field.
+     *
+     * @param AI Si le joueur est une IA.
+     * @since 1.0
+     */
+    public void setAI(boolean AI) {
+        this.AI = AI;
     }
 }
