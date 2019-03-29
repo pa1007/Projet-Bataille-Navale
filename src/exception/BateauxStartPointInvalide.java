b9ebe88121f2e31647eddb112e173d28029ac924
pa1007 <pa.fourriere@sfr.fr>
2019-03-29 19:23:45 +0100

    Refactor du message

--- src/exception/BateauxStartPointInvalide.java
+++ src/exception/BateauxStartPointInvalide.java
@@ -3,7 +3,7 @@ package exception;
 public class BateauxStartPointInvalide extends Exception {
 
     public BateauxStartPointInvalide() {
-        super("la place du bateau n'est pas valide (en dehors ou sur un autre bateau");
+        super("la place du bateau n'est pas valide (en dehors, sur un autre bateau ou Obstuer)");
     }
 
     public BateauxStartPointInvalide(String message) {
