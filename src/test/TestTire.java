3840f1b3fe3051e45601b39a88915392d5730b1d
Paul <louppe7u@etu.univ-lorraine.fr>
2019-04-05 15:24:35 +0200

    partie de testTire

--- src/test/TestTire.java
+++ src/test/TestTire.java
@@ -1,11 +1,27 @@
 package test;
 
 import org.junit.Test;
+
+import jeux.Place;
+import jeux.Tire;
+
 import static org.junit.Assert.*;
 
 public class TestTire {
 
     @Test
-    public void testToString() {
+    public void testToStringTouche() {
+    	Place p = new Place("A6");
+    	Tire t = new Tire(true, p);
+    	
+    	assertEquals("Mauvais symbole dans la console","┼",t.toString() );
+    }
+    
+    @Test
+    public void testToStringNonTouche() {
+    	Place p = new Place("A6");
+    	Tire t = new Tire(false, p);
+    	
+    	assertEquals("Mauvais symbole dans la console","Ø",t.toString() );
     }
 }
