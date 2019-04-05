8c8c7e5a9ad1b5ba9d5d7cd6bba9983f561fe697
Paul <louppe7u@etu.univ-lorraine.fr>
2019-04-05 15:23:41 +0200

    partie de testGrille

--- src/test/TestGrille.java
+++ src/test/TestGrille.java
@@ -2,6 +2,16 @@ package test;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.*;
+
+import jeux.Grille;
+import jeux.Jeux;
+import jeux.ModeDeJeux;
+import jeux.Place;
+import utils.Player;
+
 public class TestGrille {
 
     @Test
@@ -9,7 +19,45 @@ public class TestGrille {
     }
 
     @Test
-    public void inBound() {
+    public void inBoundNormal() {
+    	Player p  = new Player();
+    	List<Player> pL = new ArrayList<>();
+        pL.add(p);
+    	Jeux          j = new Jeux(ModeDeJeux.MONO_JOUEUR, pL);
+        Grille        g = new Grille(15, 15, j, p);
+        Place temp = new Place("A6");
+        
+        boolean res = g.inBound(temp);
+        
+        assertEquals("La place doit etre dans la grille",true, res);
+    }
+    
+    @Test
+    public void inBoundOversizedXaxe() {
+    	Player p  = new Player();
+    	List<Player> pL = new ArrayList<>();
+        pL.add(p);
+    	Jeux          j = new Jeux(ModeDeJeux.MONO_JOUEUR, pL);
+        Grille        g = new Grille(15, 15, j, p);
+        Place temp = new Place("A55");
+        
+        boolean res = g.inBound(temp);
+        
+        assertEquals("La place ne doit pas etre dans la grille",false, res);
+    }
+    
+    @Test
+    public void inBoundOversizedYaxe() {
+    	Player p  = new Player();
+    	List<Player> pL = new ArrayList<>();
+        pL.add(p);
+    	Jeux          j = new Jeux(ModeDeJeux.MONO_JOUEUR, pL);
+        Grille        g = new Grille(15, 15, j, p);
+        Place temp = new Place("Z1");
+        
+        boolean res = g.inBound(temp);
+        
+        assertEquals("La place ne doit pas etre dans la grille",false, res);
     }
 
     @Test
