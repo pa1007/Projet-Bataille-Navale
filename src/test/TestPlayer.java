96b0ff2039ba68889f2ee8206a64bdf15c4c4691
Paul <louppe7u@etu.univ-lorraine.fr>
2019-04-06 10:31:54 +0200

    test randBetween, test player

--- src/test/TestPlayer.java
+++ src/test/TestPlayer.java
@@ -1,30 +1,53 @@
 package test;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
+
+import utils.Player;
 
 public class TestPlayer {
 
-    @Test
-    public void placerBateau() {
-    }
+	@Test
+	public void placerBateau() {
+	}
 
-    @Test
-    public void play() {
-    }
+	@Test
+	public void play() {
+	}
 
-    @Test
-    public void obstruerCase() {
-    }
+	@Test
+	public void obstruerCase() {
+	}
 
-    @Test
-    public void randBetween() {
-    }
+	@Test
+	public void randBetween() {
+		Player p = new Player();
+		boolean un = false;
+		boolean deux = false; 
+		boolean trois = false;
+		for(int i = 0; i<50; i++) {
+			int test = p.randBetween(1, 3);
+			switch(test) {
+				case 1:
+					un = true;
+					break;
+				case 2:
+					deux = true; 
+					break; 
+				case 3:
+					trois = true;
+					break;
+			}
+		}
+		assertTrue("random mauvais", un && deux && trois);
+	}
 
-    @Test
-    public void placerBateauRandom() {
-    }
+	@Test
+	public void placerBateauRandom() {
+	}
 
-    @Test
-    public void getRandomPlace() {
-    }
+	@Test
+	public void getRandomPlace() {
+	}
 }
