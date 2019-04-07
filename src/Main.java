06d8fb463aa5b8065cacbd1ec5c1255f9f41926a
Paul <louppe7u@etu.univ-lorraine.fr>
2019-04-07 12:50:14 +0200

    regles du jeu dans la console et merge des mains

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import exception.GrilleNonCreeException;
 import exception.LoadSaveException;
+import graphic.GraphicMain;
 import jeux.Grille;
 import jeux.Jeux;
 import jeux.ModeDeJeux;
@@ -28,9 +29,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Bienvenue dans la bataille navale :");
         System.out.println("1 : MonoJoeur");
+        System.out.println("2 : Lancer une partie en mode graphique!!");
         System.out.println("9 : Changer le path de sauvegarde (A garder pour retrouver les sauvegaedes )");
         System.out.println("10 : Reprendre la derniere partie from savedFile");
         System.out.println("11 : Reprendre la derniere partie from un autre fichier");
+        System.out.println("50 : Regles du jeux et codes couleurs de la partie graphique");
         System.out.println("100 : Quitter");
         choix = sc.nextInt();
         sc.nextLine();
@@ -42,6 +45,10 @@ public class Main {
                 modifTailleGrille(j);
                 j.lancerPartie();
                 break;
+            case 2:
+            	GraphicMain main = new GraphicMain();
+                main.launchGame();
+            	break;
             case 9:
                 System.out.println("Le path de la sauvegarde :");
                 SavedObject.changePath(sc.nextLine());
@@ -54,6 +61,9 @@ public class Main {
                 SavedObject o = SavedObject.load();
                 o.getJeux().reprendrePartie();
                 break;
+            case 50:
+            	reglesDuJeux();
+            	break;
             case 100:
                 System.exit(0);
 
@@ -82,6 +92,33 @@ public class Main {
             Grille g = new Grille(horizontale, verticale, j, p);
             p.setGrille(g);
         }
+    }
+    
+    public static void reglesDuJeux() {
+    	StringBuffer b = new StringBuffer();
+    	String string;
+    	//regles console
+    	string = "\n BIENVENUE DANS LA BATAILLE NAVALE !";
+    	string += "\n Partie console :\n" + 
+    			"Dans la console vous pouvez jouer en monojoueur uniquement. \n" + 
+    			"Symboles :\n";
+    	string += "- « 0 » : Quand la case est inconnue\n" + 
+    			"- « p » : Quand la case est obstruee ‘ile par exemple‘\n" + 
+    			"- « B » : Quand la case est occupee par un bateau\n" + 
+    			"- « ┼ » : Quand un bateau a ete touche\n" + 
+    			"- « Ø » : Quand vous avez touche l’eau";
+    	
+    	//regles graphique
+    	string += "\n\nPartie graphique :\n" + 
+    			"Dans la partie graphique vous pouvez jouer en monojoueur ou bien contre l’intelligence artificelle « VS_IA »\n" + 
+    			"Vous pouvez aussi placer vos bateaux de maniere aleatoire.\n" + 
+    			"Code Couleur :\n" + 
+    			"- case bleu clair : Quand la case est inconnue « eau »\n" + 
+    			"- croix rouge = Quand vous avez touche l’eau\n" + 
+    			"- croix rouge sur fond noir = Quand un bateau a ete touche\n" + 
+    			"- (mode ia seulement) case bleu fonce = Quand l’ia a tire sur votre grille";
+    	b.append(string);
+    	System.out.println(b);
     }
 
 }
